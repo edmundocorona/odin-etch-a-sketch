badbaf989e7f66537c3d7f3680de634cbe2283b9
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-26 00:47:16 -0600

    Add a button to create a grid with input n squares

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-function createGrid () {
-  const ROWS = 16;
-  const COLS = 16;
+function createGrid (numberOfSquares = 16) {
+  const ROWS = numberOfSquares;
+  const COLS = numberOfSquares;
   const container = document.createElement('div');
   container.setAttribute('id', 'container');
   for (let row = 0; row < ROWS; row++) {
@@ -17,17 +17,56 @@ function createGrid () {
   }
   const body = document.querySelector('body');
   body.appendChild(container);
-}
 
-createGrid();
-
-const listCells = document.querySelectorAll('.cell');
-listCells.forEach((cell) => {
+  const listCells = document.querySelectorAll('.cell');
+  listCells.forEach((cell) => {
   cell.addEventListener('mouseleave', () => {
     changeBgColor(cell);
   });
 });
+}
+
+createGrid();
+
+
 
 function changeBgColor (element) {
   element.classList.add('hovered');
+}
+
+const btnEditGrid = document.querySelector('#editGrid');
+btnEditGrid.addEventListener(('click'), () => {
+  const numberOfSquares = getNumberSquares();
+  deleteGrid()
+  updateCellStyle(numberOfSquares)
+  createGrid(numberOfSquares)
+});
+
+function getNumberSquares () {
+  const numberOfSquares = prompt('How many squares per side do you want?')
+  return 1 <= numberOfSquares && numberOfSquares <= 100
+    ? numberOfSquares : getNumberSquares();
+}
+
+function updateCellStyle (numberOfSquares = 16) {
+  const stylesheet = document.styleSheets[0];
+  const sizeSquare = Math.floor(100 / numberOfSquares * 100) / 100;
+  console.log(sizeSquare);
+  let cellCSSRules;
+
+  for(let i = 0; i < stylesheet.cssRules.length; i++) {
+    if (stylesheet.cssRules[i].selectorText === '.cell') {
+      cellCSSRules = stylesheet.cssRules[i];
+      break;
+    }
+  }
+
+  cellCSSRules.style.setProperty('width', `${sizeSquare}vw`);
+  cellCSSRules.style.setProperty('height', `${sizeSquare}vw`);
+}
+
+function deleteGrid () {
+  const body = document.querySelector('body');
+  const container = document.querySelector('#container')
+  body.removeChild(container);
 }
