c28b9895c3c6b4b35ade5f90f177ce2c776ff0fd
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-25 21:38:53 -0600

    update: create a 16x16 grid if square divs

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function createGrid () {
+  const ROWS = 16;
+  const COLS = 16;
+  const container = document.createElement('div');
+  container.setAttribute('id', 'container');
+  for (let row = 0; row < ROWS; row++) {
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    divRow.setAttribute('id', `r${row}`)
+    for (let col = 0; col < COLS; col++) {
+      const divCol = document.createElement('div');
+      divRow.appendChild(divCol);
+      divCol.classList.add('cell');
+      divCol.setAttribute('id',`r${row}-c${col}`);
+    }
+    container.appendChild(divRow);
+  }
+  const body = document.querySelector('body');
+  body.appendChild(container);
+}
+
+createGrid();
